c9760db13d661fb2c5cf26335f81db4781f3f23f
carboni-rob <roberto.carboni@laduma.com>
2021-08-21 10:52:30 +0100

    Displayed sequence working

---
+++ src/App.styles.tsx
@@ -0,0 +1,95 @@
+import { Button, Slider } from "@material-ui/core";
+import styled from "styled-components";
+
+interface GenericProps {
+  $isVisible: boolean;
+}
+
+interface AnswerProps {
+  $isVisible: boolean;
+  isCorrect: boolean;
+  isRevealed: boolean;
+}
+
+export const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #001529;
+`;
+
+export const MainTitle = styled.h1`
+  margin-bottom: 2em;
+`;
+
+export const ControlsContainer = styled.div`
+  margin: 1em 0;
+  width: 100%;
+  max-width: 22em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5;
+`;
+
+export const SliderContainer = styled.div`
+  margin: 0 0 1em 0;
+`;
+
+export const RangeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RangeInput = styled.input`
+  width: 2.5em;
+  margin: 0.5em;
+  font-size: large;
+`;
+
+export const Sequence = styled.h2<GenericProps>`
+  visibility: ${({ $isVisible: visible }) => getVisibility(visible)};
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  padding: 2em;
+`;
+
+export const AnswerContainer = styled.input<AnswerProps>`
+  height: 2em;
+  width: 2em;
+  margin: 1em;
+  visibility: ${({ $isVisible: visible }) => getVisibility(visible)};
+  border: ${({ isCorrect, isRevealed }) => getBorder(isCorrect, isRevealed)};
+  box-sizing: border-box;
+  font-size: large;
+
+  &:disabled {
+    color: white;
+  }
+
+  &::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const RevealButton = styled(Button)<GenericProps>`
+  visibility: ${({ $isVisible: visible }) => getVisibility(visible)};
+`;
+
+const getVisibility = (isVisible: boolean): string =>
+  isVisible ? "visible" : "hidden";
+
+const getBorder = (isCorrect: boolean, isRevealed: boolean): string => {
+  if (!isRevealed) return "none";
+  return isCorrect ? "2px solid green" : "2px solid red";
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Slider, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import {
+  AnswerContainer,
+  AppContainer,
+  ControlsContainer,
+  InnerContainer,
+  MainTitle,
+  RangeContainer,
+  RangeInput,
+  RevealButton,
+  Sequence,
+  SliderContainer,
+} from "./App.styles";
 
 function App() {
+  const [memoryTime, setMemoryTime] = useState(5);
+  const [sequence, setSequence] = useState<number[]>([0]);
+  const [sequenceLength, setSequenceLength] = useState(6);
+  const [range, setRange] = useState("9");
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isSequenceVisible, setSequenceVisible] = useState(false);
+  const [isAnswerVisible, setAnswerVisible] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [answer, setAnswer] = useState<(string | null)[]>([]);
+
+  useEffect(() => {
+    if (!timeLeft) {
+      if (gameStarted) {
+        setSequenceVisible(false);
+        setAnswerVisible(true);
+      }
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeLeft, sequence]);
+
+  const generateSequence = () => {
+    const newSequence: number[] = [];
+    for (let i = 1; i <= sequenceLength; i++) {
+      newSequence.push(Math.floor(Math.random() * parseInt(range)));
+    }
+
+    setAnswer(new Array(sequenceLength).fill(""));
+    setSequence(newSequence);
+    setTimeLeft(memoryTime);
+    setSequenceVisible(true);
+    setAnswerVisible(false);
+    setGameStarted(true);
+  };
+
+  const handleAnswer = (index: number, value: string) => {
+    const newAnswer = [...answer];
+    newAnswer[index] = value;
+    setAnswer(newAnswer);
+  };
+
+  const checkCorrectedness = (index: number): boolean =>
+    sequence[index]?.toString() === answer[index];
+
+  const renderAnswerInputs = () => {
+    return (
+      <div>
+        {answer.map((_, index) => (
+          <AnswerContainer
+            key={index}
+            value={answer[index] as string}
+            disabled={isSequenceVisible}
+            $isVisible={isAnswerVisible}
+            type="number"
+            onChange={(event) => handleAnswer(index, event.target.value)}
+            isCorrect={checkCorrectedness(index)}
+            isRevealed={isSequenceVisible}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const revealAnswer = () => {
+    setSequenceVisible(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AppContainer>
+      <InnerContainer>
+        <MainTitle>SEQUENCE MEMORY v.2</MainTitle>
+
+        <ControlsContainer>
+          <SliderContainer>
+            <Slider
+              value={sequenceLength}
+              aria-labelledby="length-selector"
+              step={1}
+              min={1}
+              max={14}
+              valueLabelDisplay="auto"
+              onChange={(_, value) => setSequenceLength(value as number)}
+            />
+            <Typography id="length-selector" gutterBottom>
+              Sequence Length: {sequenceLength} numbers
+            </Typography>
+          </SliderContainer>
+
+          <SliderContainer>
+            <Slider
+              value={memoryTime}
+              aria-labelledby="time-selector"
+              step={5}
+              min={5}
+              max={60}
+              valueLabelDisplay="auto"
+              onChange={(_, value) => setMemoryTime(value as number)}
+            />
+            <Typography id="time-selector" gutterBottom>
+              Memorisation Time: {memoryTime} secs
+            </Typography>
+          </SliderContainer>
+
+          <RangeContainer>
+            <span>Numbers range: 0 to </span>
+            <RangeInput
+              type="number"
+              value={range}
+              onChange={(event) => setRange(event.target.value)}
+            />
+          </RangeContainer>
+        </ControlsContainer>
+
+        <Button variant="contained" onClick={generateSequence}>
+          Generate Sequence
+        </Button>
+
+        <Sequence $isVisible={isSequenceVisible}>
+          {sequence.join(" - ")}
+        </Sequence>
+
+        {renderAnswerInputs()}
+
+        <h2>Time remaining: {timeLeft}</h2>
+
+        <RevealButton
+          variant="contained"
+          onClick={revealAnswer}
+          $isVisible={!isSequenceVisible && gameStarted}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Reveal Answer
+        </RevealButton>
+      </InnerContainer>
+    </AppContainer>
   );
 }
 
